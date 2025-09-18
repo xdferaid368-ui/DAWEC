b1ecd9ea6ed015cc488dc2e6cc51d5554927645c
xdferaid368-ui <xdferaid368@ieshnosmachado.org>
2025-09-18 14:49:01 +0200

    Add initial HTML and JavaScript files for Ejemplo 1

---
+++ Clase18-09/ejemplo1.js
@@ -0,0 +1,6 @@
+document.getElementById("salida").innerHTML = "Hola Mundo desde JavaScript";
+let num = prompt("Dame un numero: ");
+let resultado = num * 2;
+document.getElementById("salida").innerHTML += `<br>El doble de ${num} es ${resultado}`;
+// Clase18-09/ejemplo1.js
+// Archivo JavaScript externo referenciado en un archivo HTML   
