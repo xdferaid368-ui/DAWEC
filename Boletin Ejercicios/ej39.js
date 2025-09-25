e4d5f3fe025978e5cfc992982a3a2780cce656cc
xdferaid368-ui <xdferaid368@ieshnosmachado.org>
2025-09-25 13:35:19 +0200

    25/09

---
+++ Boletin Ejercicios/ej39.js
@@ -0,0 +1,17 @@
+// Realiza un programa que nombre los 100 primeros numeros primos 
+function esPrimo(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+let contador = 0;
+let numero = 2;
+while (contador < 100) {
+    if (esPrimo(numero)) {
+        console.log(numero);
+        contador++;
+    }
+    numero++;
+}
