765784e8e66d79a2dc424d719c1bce110fd1f8e7
xdferaid368-ui <xdferaid368@ieshnosmachado.org>
2025-12-11 14:20:01 +0100

    Litle things

--- UD3/Boletin1/ej10.js
+++ UD3/Boletin1/ej10.js
@@ -4,7 +4,6 @@ const selectPrioridad = document.querySelector('select[name="prioridad"]');
 const boton = document.getElementById("boton");
 const tbody = document.getElementById("tbody");
 
-
 const prioridadValor = {
   "Muy alta": 1,
   "Alta": 2,
@@ -12,11 +11,7 @@ const prioridadValor = {
   "Baja": 4,
   "Muy baja": 5
 };
-
-// Array donde guardamos las tareas
 let tareas = [];
-
-// Añadir tarea
 boton.addEventListener("click", () => {
   const texto = inputTarea.value.trim();
   const prioridad = selectPrioridad.value;
@@ -25,25 +20,16 @@ boton.addEventListener("click", () => {
     alert("Debes escribir una tarea.");
     return;
   }
-
-  // Guardar la tarea en el array
   tareas.push({
     texto,
     prioridad,
     orden: prioridadValor[prioridad]
   });
-
-  // Ordenar según prioridad
   tareas.sort((a, b) => a.orden - b.orden);
-
-  // Pintar la tabla
   pintarTabla();
-
-  // Limpiar campo
   inputTarea.value = "";
 });
 
-// Función que pinta la tabla completa
 function pintarTabla() {
   tbody.innerHTML = "";
 
@@ -54,20 +40,8 @@ function pintarTabla() {
       <td>${index + 1}</td>
       <td>${tarea.texto}</td>
       <td>${tarea.prioridad}</td>
-      <td style="text-align:center; cursor:pointer;">
-        <i class="fa fa-trash w3-text-red" data-index="${index}"></i>
-      </td>
     `;
 
     tbody.appendChild(tr);
-  });
-
-  // Activar el evento de borrar para cada papelera
-  document.querySelectorAll(".fa-trash").forEach(icon => {
-    icon.addEventListener("click", () => {
-      const i = icon.getAttribute("data-index");
-      tareas.splice(i, 1);
-      pintarTabla();
-    });
   });
 }
